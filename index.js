45ffc2271917bc8d7b5a1e12651c7f5a59d757a1
AM-Game <78294729+AM-Game@users.noreply.github.com>
2021-04-19 15:12:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://elated-gray-emoji.glitch.me"]
+const urls = ["https://raspy-berry-coil.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
