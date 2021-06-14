61555fe15ee2e0b34d76a7fd9993215d33a4b5ba
AM-Game <78294729+AM-Game@users.noreply.github.com>
2021-06-14 18:43:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://raspy-berry-coil.glitch.me"]
+const urls = ["https://replit.com/@AMGame/Aril-Bot-Discord-Indonesia"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
